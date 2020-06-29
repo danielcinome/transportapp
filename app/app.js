9f11b452ccd0a9339a5ff2d38db6483c9f8ccf6e
danielcinome <danielchinome987@gmail.com>
2020-06-29 01:05:18 +0000

    fix path for view

--- app/app.js
+++ app/app.js
@@ -13,9 +13,9 @@ app.use((req, res, next) => {
 });
 
 // Use for server the view
-app.use(express.static('../view'));
+app.use(express.static((__dirname + '../view'));
 
-// declare the route for API 
+// declare the route for API
 const route = require('./routes');
 app.use('/api', route)
 
