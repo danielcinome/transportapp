683f3fba6ba69dac57245ff94da2040c1844f3d0
danielcinome <danielchinome987@gmail.com>
2020-06-29 00:02:11 +0000

    app contains the functional files, routes are defined for the API, processes the queries and returns the required information

---
+++ app/app.js
@@ -0,0 +1,24 @@
+// Express is a web application infrastructure Node.js
+const express = require('express');
+
+let app = express();
+
+// this is use for fix problem with of CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+// Use for server the view
+app.use(express.static('../view'));
+
+// declare the route for API 
+const route = require('./routes');
+app.use('/api', route)
+
+app.listen(8000, function() {
+    console.log("Server up and listening");
+  });
---
+++ app/routes/vehiclels.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const request = require('request');
+const xml2js = require('xml2js');
+
+
+const urlVehicleId = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocation&a=sf-muni&v=';
+
+/**
+ * @description Function used to parse the XML format obtained as a response from the NetBusXML API to JSON
+ * @param {XML} data in formation xml
+ * @return returns information in JSON format
+ */
+function xmltoJson(body) {
+    let json = null;
+    xml2js.parseString(body, { mergeAttrs: true }, (err, result) => {
+    if (err) {
+        throw err;
+    }
+    json = result;
+  });
+  return json;
+};
+
+
+/**
+ * @description Route get, allows to obtain information about the location of a vehicle
+ * @route /api/vehicles/{id} expample value : 1453
+ * @param {req} represents the (request)
+ * @param {res} represents the (response)
+ */
+router.get('/hola/:id', function (req, res) {
+
+  const vehicleId = req.params.id;
+  let listPredict = [];
+
+    request(urlVehicleId + vehicleId, function (err, response, body) {
+      if (err) {
+        return console.error(err);
+      }
+      const json = xmltoJson(body);
+      res.send(json);
+    });
+});
+
+module.exports = router;
